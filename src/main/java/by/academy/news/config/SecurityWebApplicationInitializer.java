04f9e75a9b2d72fd647454353fea4f09e44c6167
JohnyRazDva <39062373+JohnyRazDva@users.noreply.github.com>
2022-10-24 20:04:59 +0300

    Delete src/main directory

--- src/main/java/by/academy/news/config/SecurityWebApplicationInitializer.java
+++
@@ -1,9 +0,0 @@
-package by.academy.news.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-/**
- * @author Eugene Petrov
- */
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-}
